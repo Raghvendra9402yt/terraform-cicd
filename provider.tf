86d5b09e9c6f45257e72353187c45cf0590fc22f
Raghvendra9402 <singhraghvendra405@gmail.com>
2026-07-28 21:58:19 +0530

    init

---
+++ provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+  backend "s3" {
+    bucket = "terraform-state-rs-yt"
+
+    key = "tf-ci-cd/terraform.tfstate"
+    region = "ap-south-1"
+    use_lockfile = true
+    encrypt = true
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+}
